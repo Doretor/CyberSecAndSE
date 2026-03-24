f3d43ec9ed27bbcf342a2a6ee44ab070e646c73a
Doretor <filippyrek60@gmail.com>
2026-03-24 13:31:50 +0100

    cd lab2

--- scripts/export_warn_timestamps.sh
+++ scripts/export_warn_timestamps.sh
@@ -1,2 +1,4 @@
-grep -o ".*WARN" ../logs/*.log > ../reports/warn_timestamps.txt
-#grep -o ".logs:.*" ../reports/all_errors.txt > ../reports/warn_timestamps.txt
+grep WARN ../logs/*.log > ../reports/warn_timestamps.txt
+cut -d ":" -f 2,3,4 ../reports/warn_timestamps.txt > temp.txt
+cut -d " " -f 1,2 ./temp.txt > ../reports/warn_timestamps.txt
+rm temp.txt
